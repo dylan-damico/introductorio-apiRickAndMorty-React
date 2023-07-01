07f9381e6cc09b33f682d3afe8ad29a60b3a48ef
Dylan D'Amico <dylandamico4@gmail.com>
2023-06-30 23:12:05 -0300

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import imageRickMorty from "./img/rick-morty.png";
 import './App.css';
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={} alt="Rick & Morty" className='img-home'/>
+        <Characters/>
+        <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
+        <button onClick={reqApi} className="btn-search">Buscar personajes</button>
       </header>
     </div>
   );
---
+++ src/components/Characters.js
@@ -0,0 +1,8 @@
+
+export default function Characters(){
+    return(
+        <div>
+            <h1>PLACEHOLDER</h1>
+        </div>  
+    ) 
+}
