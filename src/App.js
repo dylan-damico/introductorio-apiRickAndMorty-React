1f9d50342c7f659175b617c35cc15a16a8079cf2
Dylan D'Amico <dylandamico4@gmail.com>
2023-08-28 01:49:54 -0300

    commit1

--- src/App.js
+++ src/App.js
@@ -17,9 +17,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <Characters/>
-        <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
-        <button onClick={reqApi} className="btn-search">Buscar personajes</button>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          <>
+          <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
+          <button onClick={reqApi} className="btn-search">Buscar personajes</button>
+          </>
+        )}               
       </header>
     </div>
   );
